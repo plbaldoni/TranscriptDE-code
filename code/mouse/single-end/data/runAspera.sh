54b7b14af6f7fb38d5a3d4a5851f6d852b1a658f
Pedro Baldoni <pedrobaldoni@gmail.com>
2023-02-24 08:51:26 +1100

    Fixing destination

--- code/mouse/single-end/data/runAspera.sh
+++ code/mouse/single-end/data/runAspera.sh
@@ -12,7 +12,7 @@ tmpdir=.
 key=/stornext/System/data/tools/aspera/aspera-3.9.6/etc/asperaweb_id_dsa.openssh
 
 prj=../../../../data/mouse/single-end/misc/filereport_read_run_PRJNA258286_tsv.txt
-
+dest=../../../../data/mouse/single-end/fastq/
 path=( $(awk -v FS='\t' -v OFS='\t' '{ print $31 }' $prj | tail -n +2 | awk -v FS=';' -v OFS=';' '{print $1}') )
 alias=( $(awk -v FS='\t' -v OFS='\t' '{ print $26 }' $prj | tail -n +2) )
 len=${#alias[@]}
@@ -23,7 +23,7 @@ for i in $(seq 1 $len) ; do
 
   aliasgz=${alias[$j]}.fastq.gz
 
-  to=${aliasgz}
+  to=${dest}${aliasgz}
 
   echo "Downloading $from..."
   ascp -QT -l 300m -P33001 -i $key era-fasp@$from $to
